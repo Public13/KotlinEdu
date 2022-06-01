1833086175e5a120b3132c76ffec056ee3cf520d
pelle <pelle@protei-lab.ru>
2022-06-01 06:24:06 +0300

    listExt fixed drop

--- src/main/kotlin/list/ListEx.kt
+++ src/main/kotlin/list/ListEx.kt
@@ -1,14 +1,18 @@
 package list
 
-sealed class MyList<T>
+sealed class MyList<T> {
+    companion object {
+        @Suppress("UNCHECKED_CAST")
+        fun <T> of(vararg values: T): MyList<T> = when (values.isEmpty()) {
+            true -> Empty() as MyList<T>
+            false -> Cons(values.first(), of(*values.copyOfRange(1, values.size)))
+        }
+    }
+}
+
 class Empty : MyList<Nothing>()
 class Cons<T>(val value: T, val tail: MyList<T>) : MyList<T>()
 
-@Suppress("UNCHECKED_CAST")
-fun <T> of(vararg values: T): MyList<T> = when (values.isEmpty()) {
-    true -> Empty() as MyList<T>
-    false -> Cons(values.first(), of(*values.copyOfRange(1, values.size)))
-}
 
 fun <T> MyList<T>.head() = when (this) {
     is Cons -> value
@@ -21,12 +25,23 @@ fun <T> length(n: Int, list: MyList<T>): Int {
     return length(n + 1, list2.tail)
 }
 
-fun <T>drop(n: Int, list: MyList<T>): MyList<T> = when {
+fun <T> drop(n: Int, list: MyList<T>): MyList<T> = when {
     n <= 0 -> list
     else -> when (list) {
         is Cons -> drop(n - 1, list.tail)
         is Empty -> list
     }
+}
+
+fun <T> MyList<T>.drop2(n: Int): MyList<T> {
+    tailrec fun go(n: Int, list: MyList<T>): MyList<T> = when {
+        n <= 0 -> list
+        else -> when (list) {
+            is Cons -> go(n - 1, list.tail)
+            is Empty -> list
+        }
+    }
+    return go(n, this)
 }
 
 fun <T> MyList<T>.dropWhile(f: (T) -> Boolean): MyList<T> = when (this) {
@@ -37,9 +52,9 @@ fun <T> MyList<T>.dropWhile(f: (T) -> Boolean): MyList<T> = when (this) {
 //fun printList(list: MyList)
 
 fun main() {
-    val list = of(1, 2, 3, 4, 5)
+    val list = MyList.of(1, 2, 3, 4, 5)
     println(length(0, list))
-    val list2 = drop(1, list)
+    val list2 = list.drop2(1)
     println(length(0, list2))
 
 //    val list = listOf<Int>(1, 2, 3, 4)
