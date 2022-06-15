4e1d495edb432b8346d99630414251b0d89f312e
13pub <13public@gmail.com>
2022-06-15 22:24:26 +0300

    streams dz3

--- src/main/kotlin/list/Stream.kt
+++ src/main/kotlin/list/Stream.kt
@@ -18,7 +18,7 @@ fun <T> Stream<T>.toList(): MyList<T> = when (this) {
 }
 
 fun <T> Stream<T>.take(n: Int): Stream<T> = when {
-    n <= 0 -> Stream.Empty() as Stream<T>
+    n <= 0 -> Stream.Empty()
     else -> when (this) {
         is Stream.Empty -> this
         is Stream.Cons -> Stream.Cons(this.value) { this.tail().take(n - 1) }
@@ -27,13 +27,60 @@ fun <T> Stream<T>.take(n: Int): Stream<T> = when {
 
 fun <T> Stream<T>.takeWhile(f: (T) -> Boolean): Stream<T> =
     when (this) {
-        is Stream.Empty -> Stream.Empty() as Stream<T>
+        is Stream.Empty -> this
         is Stream.Cons -> when (f(this.value())) {
             true -> Stream.Cons(this.value) { this.tail().takeWhile(f) }
-            false -> this
+            false -> Stream.Empty()
         }
     }
 
+fun <T> Stream<T>.drop(n: Int): Stream<T> = when {
+    n <= 0 -> {
+        println("n <= 0. return this")
+        this
+    }
+    else -> when (this) {
+        is Stream.Empty -> {
+            println("stream is empty. return this")
+            this
+        }
+        is Stream.Cons -> when (this.tail()) {
+            is Stream.Empty -> {
+                println("tail stream is empty. return this")
+                Stream.Empty()
+            }
+            is Stream.Cons -> {
+                println("tail stream is cons. construct new stream")
+                Stream.Cons((this.tail() as Stream.Cons<T>).value, { this.tail().drop(n - 1) })
+            }
+        }
+    }
+}
+
+//fun <T, V> MyList<T>.foldRight(v: V, f: (T, V) -> V): V = when (this) {
+//    is Empty -> v
+//    is Cons -> f(this.value, this.tail.foldRight(v, f))
+//}
+
+// это не правильно
+fun <T, V> Stream<T>.foldRight(v: V, f: (T, V) -> V): V = when (this) {
+    is Stream.Empty -> v
+    is Stream.Cons -> f(this.value(), this.tail().foldRight(v, f))
+}
+
+fun main() {
+    val stream = Stream.of(2, 2, 5, 1)
+    stream.toList().forEach { println(it) }
+
+//    println("=== take")
+//    stream.take(2).toList().forEach { println(it) }
+
+    println("=== drop")
+    stream.drop(1).toList().forEach { println(it) }
+
+//    println("=== take while")
+//    stream.takeWhile { it == 2 }.toList().forEach { println(it) }
+}
 
 //fun <T> MyList<T>.drop(n: Int): MyList<T> = when {
 //    n <= 0 -> this
@@ -43,29 +90,5 @@ fun <T> Stream<T>.takeWhile(f: (T) -> Boolean): Stream<T> =
 //    }
 //}
 
-//fun <T> Stream<T>.drop(n: Int): Stream<T> = when {
-//    n > 0 -> when (this) {
-//        is Stream.Empty -> this
-////        is Stream.Cons -> Stream.Cons(this.value) { this.tail().drop(n - 1) }
-//        is Stream.Cons -> when (this.tail()) {
-//            is Stream.Empty -> Stream.Empty()
-//            is Stream.Cons -> Stream.Cons((this.tail() as Stream.Cons<T>).value) { this.tail().drop(n - 1) }
-//        }
-//    }
-//    else -> this
-//}
-
-
 //takeWhile(()->Bool)
 //foldRight
-
-fun main() {
-    val stream = Stream.of(1, 2, 3)
-    stream.toList().forEach { println(it) }
-    println("=== take")
-    stream.take(2).toList().forEach { println(it) }
-//    println("=== drop")
-//    stream.drop(2).toList().forEach { println(it) }
-    println("=== take while")
-    stream.takeWhile { it < 1 }.toList().forEach { println(it) }
-}
