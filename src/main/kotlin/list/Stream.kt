07988734a115803a23c7f452e64910d68111619c
pelle <pelle@protei-lab.ru>
2022-06-16 11:50:02 +0300

    streams finish

--- src/main/kotlin/list/Stream.kt
+++ src/main/kotlin/list/Stream.kt
@@ -1,5 +1,9 @@
 package list
 
+import option.None
+import option.Option
+import option.Some
+
 sealed class Stream<out T> {
     class Empty : Stream<Nothing>()
     class Cons<T>(val value: () -> T, val tail: () -> Stream<T>) : Stream<T>()
@@ -34,61 +38,74 @@ fun <T> Stream<T>.takeWhile(f: (T) -> Boolean): Stream<T> =
         }
     }
 
+// stream.map(+2).flatMap([x,x]).drop(5)
 fun <T> Stream<T>.drop(n: Int): Stream<T> = when {
-    n <= 0 -> {
-        println("n <= 0. return this")
-        this
-    }
+    n <= 0 -> this
     else -> when (this) {
-        is Stream.Empty -> {
-            println("stream is empty. return this")
-            this
-        }
-        is Stream.Cons -> when (this.tail()) {
-            is Stream.Empty -> {
-                println("tail stream is empty. return this")
-                Stream.Empty()
-            }
-            is Stream.Cons -> {
-                println("tail stream is cons. construct new stream")
-                Stream.Cons((this.tail() as Stream.Cons<T>).value, { this.tail().drop(n - 1) })
-            }
+        is Stream.Empty -> this
+        is Stream.Cons -> {
+            println("drop ${this.value()}")
+            this.tail().drop(n - 1)
         }
     }
 }
 
-//fun <T, V> MyList<T>.foldRight(v: V, f: (T, V) -> V): V = when (this) {
-//    is Empty -> v
-//    is Cons -> f(this.value, this.tail.foldRight(v, f))
-//}
-
-// это не правильно
-fun <T, V> Stream<T>.foldRight(v: V, f: (T, V) -> V): V = when (this) {
-    is Stream.Empty -> v
-    is Stream.Cons -> f(this.value(), this.tail().foldRight(v, f))
+fun <T, V> Stream<T>.foldRight(acc: () -> V, f: (T, () -> V) -> V): V = when (this) {
+    is Stream.Empty -> acc()
+    is Stream.Cons -> f(this.value()) { this.tail().foldRight(acc, f) }
 }
+
+fun Stream<out Int>.sum(): Int = foldRight({ 0 }) { i, acc -> acc() + i }
+
+fun <T> Stream<T>.forAll(f: (T) -> Boolean): Boolean = foldRight({ true }) { i, acc -> acc() && f(i) }
+
+fun <T> Stream<T>.takeWhileFl(f: (T) -> Boolean): Stream<T> = foldRight({ Stream.Empty() as Stream<T> }) { i, acc ->
+    when (f(i)) {
+        true -> Stream.Cons({ i }, acc)
+        false -> acc()
+    }
+}
+
+fun <T> Stream<T>.headOption(): Option<T> = foldRight({ None() as Option<T> }) { i, _ -> Some(i) }
+
+fun <T, V> Stream<T>.map(f: (T) -> V): Stream<V> = foldRight({ Stream.Empty() as Stream<V> }) { i, acc -> Stream.Cons({ f(i) }) { acc() } }
+
+fun <T> Stream<T>.filter(f: (T) -> Boolean): Stream<T> = foldRight({ Stream.Empty() as Stream<T> }) { i, acc ->
+    when (f(i)) {
+        true -> Stream.Cons({ i }, acc)
+        false -> acc()
+    }
+}
+
+fun <T> Stream<T>.append(f: () -> Stream<T>): Stream<T> = foldRight(f) { i, acc -> Stream.Cons({ i }, acc) }
+
+fun <T, V> Stream<T>.flatMap(f: (T) -> Stream<V>): Stream<V> = foldRight({ Stream.Empty() as Stream<V> }) { i, acc -> f(i).append(acc) }
+
+// Either
+// Left (error) (вложить)
+// Right (value)
 
 fun main() {
     val stream = Stream.of(2, 2, 5, 1)
     stream.toList().forEach { println(it) }
 
+//    println("=== sum")
+//    println(stream.sum())
+
+//    println("=== map")
+//    stream.map { it * 2 }.toList().forEach { println(it) }
+
+    println("=== filter")
+    stream.map { it * 2 }.filter { it > 4 }.toList().forEach { println(it) }
+
 //    println("=== take")
 //    stream.take(2).toList().forEach { println(it) }
 
-    println("=== drop")
-    stream.drop(1).toList().forEach { println(it) }
+//    println("=== drop")
+//    stream.drop(1);//.toList().forEach { println(it) }
 
 //    println("=== take while")
 //    stream.takeWhile { it == 2 }.toList().forEach { println(it) }
 }
 
-//fun <T> MyList<T>.drop(n: Int): MyList<T> = when {
-//    n <= 0 -> this
-//    else -> when (this) {
-//        is Cons -> this.tail.drop(n - 1)
-//        is Empty -> this
-//    }
-//}
-
-//takeWhile(()->Bool)
-//foldRight
+// trampalining
