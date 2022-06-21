1ff9514e98107479b9bf388461b101cc54aaa868
pelle <pelle@protei-lab.ru>
2022-06-21 10:33:42 +0300

    either dz2

--- src/main/kotlin/eiter/Either.kt
+++ src/main/kotlin/eiter/Either.kt
@@ -1,5 +1,9 @@
 package eiter
 
+import option.None
+import option.Option
+import option.Some
+
 sealed class Either<out E, out V> {
     class Left<E>(val value: E) : Either<E, Nothing>()
     class Right<V>(val value: V) : Either<Nothing, V>()
@@ -21,5 +25,25 @@ fun <E, V, V2> Either<E, V>.map(f: (V) -> V2): Either<E, V2> = when (this) {
 fun <E, V, V2> Either<E, V>.flatMap(f: (V) -> Either<E, V2>): Either<E, V2> = when (this) {
     is Either.Left -> this
     is Either.Right -> f(this.value)
+}
+
+fun <E, V1, V2, R> map2(a: Either<E, V1>, b: Either<E, V2>, f: (V1, V2) -> R): Either<E, R> =
+    a.flatMap { av ->
+        b.map { bv ->
+            f(av, bv)
+        }
+    }
+
+fun <E, V1, V2, R> flatMap2(a: Either<E, V1>, b: Either<E, V2>, f: (V1, V2) -> Either<E, R>): Either<E, R> =
+    a.flatMap { av -> b.flatMap { bv -> f(av, bv) } }
+
+fun <E, V> Either<E, V>.orElse(f: (V) -> Option<V>): Option<V> = when (this) {
+    is Either.Left -> None() as Option<V>
+    is Either.Right -> f(this.value)
+}
+
+fun <E, V> Either<E, V>.toOption(): Option<V> = when (this) {
+    is Either.Left -> None() as Option<V>
+    is Either.Right -> Some(this.value)
 }
 
