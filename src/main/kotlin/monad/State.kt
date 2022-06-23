6934b48a73acf55c7b3bb6183f30926626989ed8
pelle <pelle@protei-lab.ru>
2022-06-23 17:22:18 +0300

    state dz

--- src/main/kotlin/monad/State.kt
+++ src/main/kotlin/monad/State.kt
@@ -1,5 +1,10 @@
 package monad
 
+import list.Cons
+import list.Empty
+import list.MyList
+import list.foldRight
+
 // putStr(s)
 // getStr():String
 // TestData( val outputs:List<String>, inputs:List<String>)
@@ -15,6 +20,20 @@ class State<S, T>(val runState: (S) -> Pair<T, S>) {
         f(p.first).runState(p.second)
     }
 }
+
+fun <S, T, T2, R> map2(a: State<S, T>, b: State<S, T2>, f: (T, T2) -> R): State<S, R> =
+    a.flatMap { av ->
+        b.map { bv ->
+            f(av, bv)
+        }
+    }
+
+fun <S, T> MyList<State<S, T>>.sequence(): State<S, MyList<T>> = foldRight(Either.Right(Empty()) as State<S, MyList<T>>) { i, acc ->
+    map2(i, acc) { iv, ia ->
+        Cons(iv, ia)
+    }
+}
+
 //map2 (State1<T>, State2<T>, f)
 //sequense List<State> -> State<List T >
 
